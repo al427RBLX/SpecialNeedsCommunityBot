d04700dcd53cdc464fd77f82763077ebf1b6ffee
al427RBLX <146673386+al427RBLX@users.noreply.github.com>
2023-11-20 15:24:38 -0500

    Update ourmission.js

--- Commands/Information/ourmission.js
+++ Commands/Information/ourmission.js
@@ -8,7 +8,7 @@ module.exports = {
     cooldown: 10,
     execute(msg, args, cmd, client, Discord) {
         const embed = new Discord.MessageEmbed()
-        .setTitle('Official Special Needs Community Our Mission')
+        .setTitle('Special Needs Community Our Mission')
         .setDescription(`Special Needs Community is aimed to fight stereotypes and discrimination against special needs and disabled people. We hope to raise a single voice in the world to raise awareness for these specific people, and we do that by donating money to charities that support and also help special needs and disabled people, or by donating daily necessities, such as: food, clothes, etc.`)
         .setColor("BLACK")
         .setFooter('Powered by Special Needs Community')
